c1c41a1ea4410db50d0db452d466c0b8239e29ef
PatrascuAdri <adriana.patrascu9@gmail.com>
2024-05-11 00:58:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const navToggleBtn = document.querySelector('#navToggleBtn');
+const nav = document.querySelector('nav.navigation');
+const navlinks = document.querySelectorAll('nav .nav-link');
+
+navToggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+
+navlinks.forEach(link => {
+    link.addEventListener('click' , () => {
+        nav.classList.remove('active');
+    });
+});
